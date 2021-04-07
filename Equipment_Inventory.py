21ba4ee238f7317179aa7fe3752696e07aebe265
alessandroibrahim99 <38700012+alessandroibrahim99@users.noreply.github.com>
2021-04-07 16:30:51 -0400

    Update Equipment_Inventory.py

--- Equipment_Inventory.py
+++ Equipment_Inventory.py
@@ -7,8 +7,8 @@ Created on Wed Mar 24 15:23:18 2021
 import base64
 from io import BytesIO
 
-pip install xlsxwriter
-import xlsxwriter
+#pip install xlsxwriter
+#import xlsxwriter
 import streamlit as st
 import pandas as pd
 
